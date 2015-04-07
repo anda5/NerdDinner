362de70d0b45517920260bdb7769a3728eb9dd89
Anda.Lixandroiu <lix.anda@yahoo.com>
2015-04-08 01:37:57 +0300

    Add configuration and IRepository

---
+++ NerdDinner/NerdDinner/Models/IDinnerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NerdDinner.Models
+{
+    public interface IDinnerRepository
+    {
+        IQueryable<Dinner> FindAllDinners();
+        Dinner GetDinner(int n);
+        void Add(Dinner dinner);
+        void Update(Dinner dinser);
+        void Delete(Dinner dinner);
+    }
+}
